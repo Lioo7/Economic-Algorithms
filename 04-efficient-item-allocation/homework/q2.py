45060f032d5bb6f5b21b5f984331b5256f250fd8
Lioz Akirav <lioz.akirav@gmail.com>
2022-11-20 13:12:29 +0200

    add materials for week 4

---
+++ 04-efficient-item-allocation/homework/q2.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+"""
+item1's value: 10
+item2's value: 20
+item3's value: 30
+item4's value: 40
+item5's value: 50
+item6's value: 60
+item7's value: 70
+item8's value: 80
+item9's value: 90
+"""
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
