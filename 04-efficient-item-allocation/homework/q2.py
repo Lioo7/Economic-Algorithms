e41830348f6cf309e7db10b5a7bfc4a2dc6290e9
Lioz Akirav <lioz.akirav@gmail.com>
2022-11-20 23:35:31 +0200

    add q2

--- 04-efficient-item-allocation/homework/q2.py
+++ 04-efficient-item-allocation/homework/q2.py
@@ -1,23 +1,49 @@
+class Item:
+    def __init__(self, id:int, value:int):
+        self.id = id
+        self.value = value
+
+    def __str__(self):
+        return f"Item{self.id}: {self.value}"
+
+class Player:
+    def __init__(self, id:int):
+        self.id = id
+        self.items = []
+    
+    def add_item(self, item:Item):
+        self.items.append(item)
+
+    def __str__(self):
+        return f"Player{self.id}: {list(str(item) for item in self.items)}"
 
 
-
-
-
-"""
-item1's value: 10
-item2's value: 20
-item3's value: 30
-item4's value: 40
-item5's value: 50
-item6's value: 60
-item7's value: 70
-item8's value: 80
-item9's value: 90
-"""
+def greedy(players:list[Player], items:list[Item]):
+    num_of_players = len(players)
+    index = 0
+    for item in items:
+        players[index].add_item(item)
+        index = (index + 1) % num_of_players 
 
 
 def main():
-    pass
+    item1 = Item(1, 10)
+    item2 = Item(2, 20)
+    item3 = Item(3, 30)
+    item4 = Item(4, 40)
+    item5 = Item(5, 50)
+
+    items = [item1, item2, item3, item4, item5]
+    items.sort(key=lambda x: x.value, reverse=True)
+
+    player1 = Player(1)
+    player2 = Player(2)
+    
+    players = [player1, player2]
+    
+    greedy(players, items)
+    print(list(str(player) for player in players))
+
 
 if __name__ == "__main__":
     main()
